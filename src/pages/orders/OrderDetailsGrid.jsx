af35800a3e5a8645c39a7520de790968c4ec2cc4
JaydenBlunt <jjaydog7@gmail.com>
2025-12-07 20:52:47 -0800

    make add to cart feature on ordersPage work

--- src/pages/orders/OrderDetailsGrid.jsx
+++ src/pages/orders/OrderDetailsGrid.jsx
@@ -1,10 +1,22 @@
 import dayjs from 'dayjs';
 import { Fragment } from 'react';
+import axios from 'axios';
 
-export function OrderDetailsGrid({ order }) {
+
+export function OrderDetailsGrid({ order, loadCart }) {
+
   return (
     <div className="order-details-grid">
       {order.products.map((orderProduct) => {
+        const addToCart = async () => {
+          await axios.post('/api/cart-items', {
+            productId: orderProduct.product.id,
+            quantity: 1
+          });
+
+          await loadCart();
+        };
+
         return (
           <Fragment key={orderProduct.product.id}>
             <div className="product-image-container">
@@ -21,7 +33,8 @@ export function OrderDetailsGrid({ order }) {
               <div className="product-quantity">
                 Quantity: {orderProduct.quantity}
               </div>
-              <button className="buy-again-button button-primary">
+              <button className="buy-again-button button-primary"
+              onClick={addToCart}>
                 <img className="buy-again-icon" src="images/icons/buy-again.png" />
                 <span className="buy-again-message">Add to Cart</span>
               </button>
