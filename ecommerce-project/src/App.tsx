a932d658e7ced115ef5865948bcdea3aa2765eed
JaydenBlunt <jjaydog7@gmail.com>
2025-12-14 00:51:55 -0800

    Convert ecommerce project to TypeScript

---
+++ ecommerce-project/src/App.tsx
@@ -0,0 +1,37 @@
+import { Routes, Route } from 'react-router'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { HomePage } from './pages/home/HomePage';
+import { CheckoutPage } from './pages/checkout/CheckoutPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
+import { NotFound } from './pages/NotFound';
+import './App.css'
+
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  const loadCart = async () => {
+      const response = await axios.get('/api/cart-items?expand=product')
+      setCart(response.data);
+  };
+
+  useEffect(() => {
+
+     loadCart();
+  }, [])
+  
+
+  return (
+    <Routes>
+      <Route index element={<HomePage cart={cart} loadCart={loadCart}/>} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} loadCart={loadCart}/>} />
+      <Route path="orders" element={<OrdersPage cart={cart} loadCart={loadCart}/>}/>
+      <Route path="tracking/:orderId/:productId" element={<TrackingPage cart={cart}/>}></Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
+
+export default App
